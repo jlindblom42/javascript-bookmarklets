207b8b6de8d5aac76bfaf6482bdfb475e6da30ef
JDL <lindb310@gmail.com>
2024-03-04 21:27:39 -0600

    More specific query selection generation, add event dispatch to input

--- bookmarklets/form-input-bookmarklet-generator.js
+++ bookmarklets/form-input-bookmarklet-generator.js
@@ -1,5 +1,5 @@
 javascript:(function () {
-    const inputs = document.querySelectorAll('input[type=text], input[type=email], input[type=password], input[type=tel], input[type=date], textarea, input[type=radio]:checked, input[type=checkbox]:checked');
+    const inputs = document.querySelectorAll('input, textarea, input[type=radio]:checked, input[type=checkbox]:checked');
     let scriptContent = 'javascript:(function(){';
     let index = 0;
     inputs.forEach(function (input) {
@@ -12,34 +12,37 @@ javascript:(function () {
         let selector = '';
         if (input.form && (input.form.id || input.form.name)) {
             selector += input.form.tagName.toLowerCase();
-            if (input.form.id) {
-                selector += '[id=' + input.form.id + ']';
-            }
             if (input.form.name) {
                 selector += '[name=' + input.form.name + ']';
+            } else if (input.form.id) {
+                selector += '[id=' + input.form.id + ']';
             }
             selector += ' ';
         }
         selector += input.tagName.toLowerCase();
-        if (input.type) {
-            selector += '[type=' + input.type + ']';
-        }
         let notSpecific = true;
         if (input.name) {
             selector += '[name="' + input.name + '"]';
             notSpecific = false;
-        }
-        if (input.id) {
+        } else if (input.id) {
             selector += '[id="' + input.id + '"]';
             notSpecific = false;
         }
         if (notSpecific) {
             return;
         }
-        if (input.type === 'checkbox' || input.type === 'radio') {
-            scriptContent += 'const ' + inputVar + ' = document.querySelector(\'' + selector + '\'); if (' + inputVar + ') ' + inputVar + '.checked=true;';
-        } else {
-            scriptContent += 'const ' + inputVar + ' = document.querySelector(\'' + selector + '\'); if (' + inputVar + ') ' + inputVar + '.value="' + value.replace(/"/g, '\\"') + '";';
+        if (input.checked && (input.type === 'checkbox' || input.type === 'radio')) {
+            scriptContent += 'const ' + inputVar + ' = document.querySelector(\'' + selector + '\');';
+            scriptContent += 'if (' + inputVar + ') {';
+            scriptContent += inputVar + '.checked=true;';
+            scriptContent += inputVar + '.dispatchEvent(new Event(\'change\'));';
+            scriptContent += '}';
+        } else if (input.type !== 'hidden') {
+            scriptContent += 'const ' + inputVar + ' = document.querySelector(\'' + selector + '\');';
+            scriptContent += 'if (' + inputVar + ') {';
+            scriptContent += inputVar + '.value="' + value.replace(/"/g, '\\"') + '";';
+            scriptContent += inputVar + '.dispatchEvent(new Event(\'change\'));';
+            scriptContent += '}';
         }
     });
     const selects = document.querySelectorAll('select');
@@ -53,15 +56,16 @@ javascript:(function () {
         let selector = select.tagName.toLowerCase();
         if (select.name) {
             selector += '[name="' + select.name + '"]';
-        }
-        if (select.id) {
+        } else if (select.id) {
             selector += '[id="' + select.id + '"]';
         }
         var selectVar = 'select' + index;
         scriptContent += 'const ' + selectVar + ' = document.querySelector(\'' + selector + '\');';
         scriptContent += 'if (' + selectVar + ') {';
         scriptContent += 'const ' + selectVar + 'Opts = Array.from(' + selectVar + '.options).find(opt => opt.value === "' + value.replace(/"/g, '\\"') + '");';
-        scriptContent += 'if (' + selectVar + 'Opts) ' + selectVar + 'Opts.selected = true;';
+        scriptContent += 'if (' + selectVar + 'Opts) {';
+        scriptContent += selectVar + 'Opts.selected = true;';
+        scriptContent += '}';
         scriptContent += selectVar + '.dispatchEvent(new Event(\'change\'));';
         scriptContent += '}';
     });
