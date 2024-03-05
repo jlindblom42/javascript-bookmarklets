8ac90f53cd9f150afdc7812fdcff7fcc2536ddef
JDL <lindb310@gmail.com>
2024-03-04 23:03:53 -0600

    Add support for multiple inputs of the same name, and multiselects.  Capture empty values.

--- bookmarklets/form-input-bookmarklet-generator.js
+++ bookmarklets/form-input-bookmarklet-generator.js
@@ -1,14 +1,11 @@
 javascript:(function () {
-    const inputs = document.querySelectorAll('input, textarea, input[type=radio]:checked, input[type=checkbox]:checked');
-    let scriptContent = 'javascript:(function(){';
+    const inputs = document.querySelectorAll('input, textarea');
+    const selectorSet = new Set();
+    let scriptContent = '';
+    scriptContent += 'javascript:(function(){';
     let index = 0;
     inputs.forEach(function (input) {
         index++;
-        const inputVar = 'input' + index;
-        const value = input.value;
-        if (value === '') {
-            return;
-        }
         let selector = '';
         if (input.form && (input.form.id || input.form.name)) {
             selector += input.form.tagName.toLowerCase();
@@ -31,43 +28,74 @@ javascript:(function () {
         if (notSpecific) {
             return;
         }
-        if (input.checked && (input.type === 'checkbox' || input.type === 'radio')) {
-            scriptContent += 'const ' + inputVar + ' = document.querySelector(\'' + selector + '\');';
-            scriptContent += 'if (' + inputVar + ') {';
-            scriptContent += inputVar + '.checked=true;';
-            scriptContent += inputVar + '.dispatchEvent(new Event(\'change\'));';
-            scriptContent += '}';
-        } else if (input.type !== 'hidden') {
-            scriptContent += 'const ' + inputVar + ' = document.querySelector(\'' + selector + '\');';
-            scriptContent += 'if (' + inputVar + ') {';
-            scriptContent += inputVar + '.value="' + value.replace(/"/g, '\\"') + '";';
-            scriptContent += inputVar + '.dispatchEvent(new Event(\'change\'));';
-            scriptContent += '}';
+        if (selectorSet.has(selector)) {
+            return;
         }
+        selectorSet.add(selector);
+        let allInputs = document.querySelectorAll(selector);
+        if (!allInputs || allInputs.length < 1) {
+            return;
+        }
+        const inputVar = 'input' + index;
+        let filtered = [...allInputs].filter(function (nestedInput) {
+            const value = nestedInput.value;
+            const type = nestedInput.type;
+            if (type === 'checkbox' || type === 'radio') {
+                return true;
+            } else if (type !== 'hidden') {
+                return true;
+            }
+            return false;
+        });
+        if (filtered.length < 1) {
+            return;
+        }
+        let nestedIndex = 0;
+        scriptContent += 'const ' + inputVar + ' = document.querySelectorAll(\'' + selector + '\');';
+        filtered.forEach(function (nestedInput) {
+            const value = nestedInput.value || '';
+            const type = nestedInput.type;
+            const nestedInputVar = inputVar + '[' + nestedIndex + ']';
+
+            if (type === 'checkbox' || nestedInput.checked && type === 'radio') {
+                scriptContent += 'if (' + nestedInputVar + ') {';
+                scriptContent += nestedInputVar + '.checked=' + nestedInput.checked + ';';
+                scriptContent += nestedInputVar + '.dispatchEvent(new Event(\'change\'));';
+                scriptContent += '}';
+                nestedIndex++;
+            } else if (type !== 'hidden') {
+                scriptContent += 'if (' + nestedInputVar + ') {';
+                scriptContent += nestedInputVar + '.value="' + value.replace(/"/g, '\\"') + '";';
+                scriptContent += nestedInputVar + '.dispatchEvent(new Event(\'change\'));';
+                scriptContent += '}';
+                nestedIndex++;
+            }
+        });
     });
     const selects = document.querySelectorAll('select');
     index = 0;
     selects.forEach(function (select) {
         index++;
-        const value = select.value;
-        if (value === '') {
-            return;
-        }
         let selector = select.tagName.toLowerCase();
         if (select.name) {
             selector += '[name="' + select.name + '"]';
         } else if (select.id) {
             selector += '[id="' + select.id + '"]';
         }
-        var selectVar = 'select' + index;
-        scriptContent += 'const ' + selectVar + ' = document.querySelector(\'' + selector + '\');';
-        scriptContent += 'if (' + selectVar + ') {';
-        scriptContent += 'const ' + selectVar + 'Opts = Array.from(' + selectVar + '.options).find(opt => opt.value === "' + value.replace(/"/g, '\\"') + '");';
-        scriptContent += 'if (' + selectVar + 'Opts) {';
-        scriptContent += selectVar + 'Opts.selected = true;';
-        scriptContent += '}';
-        scriptContent += selectVar + '.dispatchEvent(new Event(\'change\'));';
-        scriptContent += '}';
+        if (selectorSet.has(selector)) {
+            return;
+        }
+        selectorSet.add(selector);
+        let selectedOptsSelector = selector + ' option';
+        var selectVar = 'selectOpts' + index;
+        scriptContent += 'const ' + selectVar + ' = document.querySelectorAll(\'' + selectedOptsSelector + '\');';
+        const selectedOpts = document.querySelectorAll(selectedOptsSelector);
+        selectedOpts.forEach(function (selectedOpt) {
+            if (select.multiple || selectedOpt.selected) {
+                scriptContent += selectVar + '[' + selectedOpt.index + '].selected = ' + selectedOpt.selected + ';';
+            }
+        });
+        scriptContent += 'document.querySelector(\'' + selector + '\').dispatchEvent(new Event(\'change\'));';
     });
     scriptContent += '})();';
     console.log(scriptContent);
