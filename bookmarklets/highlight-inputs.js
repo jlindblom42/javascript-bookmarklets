a4ce942e79eea13682cf451959cc1daad06c7c5e
JDL <lindb310@gmail.com>
2024-03-05 23:04:26 -0600

    Apply border instead of outline if input is non-checkable.

--- bookmarklets/highlight-inputs.js
+++ bookmarklets/highlight-inputs.js
@@ -1,11 +1,12 @@
 javascript:(function () {
     const inputs = document.querySelectorAll('input, textarea, select');
     inputs.forEach(function (input) {
-        let hasValue = !input.disabled && (input.type === 'checkbox' || input.type === 'radio' ? input.checked : input.value);
+        let isCheckable = input.type === 'checkbox' || input.type === 'radio';
+        let hasValue = !input.disabled && (isCheckable ? input.checked : input.value);
         if (hasValue) {
-            input.style.outline = '3px solid rgb(0 255 133)';
+            input.style[isCheckable ? 'outline' : 'border'] = '3px solid rgb(0 255 133)';
         } else {
-            input.style.outline = '3px solid rgb(255 0 0)';
+            input.style[isCheckable ? 'outline' : 'border'] = '3px solid rgb(255 0 0)';
         }
     });
 })();
